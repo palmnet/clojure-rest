0287e43cb971e4caebc97b3eaa9278dab06892ca
palmnet <yasushi.sugino@gmail.com>
2014-05-21 04:09:16 +0900

    Json example added

--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -1,10 +1,17 @@
 (ns clojure-rest.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [cheshire.core :refer :all]))
+
+(defn json [request]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (generate-string {:first "Yasushi" :last "Sugino"})}) 
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/json" [] json)
   (route/resources "/")
   (route/not-found "Not Found"))
 
